4296ac1dc1c489582b6aafbb56cfcae498eadedb
brendon tse and loewy malkovich <brendon@>
2019-08-28 16:37:14 -0700

    add fully functioning back and front end that is seriously lacking in styles - room for DRYing in the back end

--- js/scripts.js
+++ js/scripts.js
@@ -29,15 +29,24 @@ Player.prototype.updateCurrentScore = function(newRoll) {
   return newRoll;
 };
 
-Player.prototype.displayScores = function() {
-  var currentScoreToDisplay = this.score.text();
-  var grandTotalToDisplay = this.total.text();
+// next two methods should be combined, w/ this following code in the jQuery front end calls
+// player1.displayCurrent($("#player-1-current"))
+// player1.displayTotal($("player-1-total"));
+// player2.displayCurrent($("#player-2-current"))
+// player2.displayTotal($("player-2-total"));
+Player.prototype.displayCurrent = function(outputCurrent) {
+  outputCurrent.text(this.score);
+};
+
+Player.prototype.displayTotal = function(outputTotal) {
+outputTotal.text(this.total);
 };
 
 Player.prototype.endTurn = function() {
    this.total += this.score;
    this.score = 0;
    turnNumber ++;
+   return this.total;
 };
 
 Player.prototype.roll1 = function() {
@@ -46,7 +55,7 @@ Player.prototype.roll1 = function() {
 };
 
 Player.prototype.victory = function() {
-  if ((this.total + this.score) >= 20){
+  if ((this.total + this.score) >= 100){
     $('.victory').show();
   }
 };
@@ -62,11 +71,9 @@ var player2 = new Player("player2");
 // front end ui
 $(document).ready(function() {
 
-
   $("#roll-dice").click(function(event) {
     event.preventDefault();
     var newRoll = rollDice();
-
     if (turnNumber % 2 !== 0) {
       if (newRoll !== 1) {
         player1.updateCurrentScore(newRoll);
@@ -81,7 +88,6 @@ $(document).ready(function() {
         player2.updateCurrentScore(newRoll);
         $("#display-dice").text(newRoll);
         player2.victory();
-
       } else {
         player2.roll1();
         $("#display-dice").text(newRoll);
@@ -93,6 +99,11 @@ $(document).ready(function() {
     console.log(player1);
     console.log(player2);
     $('#current-player').html(displayPlayerFunction(turnNumber));
+    // call for display needs to be condensed
+    $("#player-1-current").text(player1.score);
+    $("#player-1-total").text(player1.total);
+    $("#player-2-current").text(player2.score);
+    $("#player-2-total").text(player2.total);
   });
 
 
@@ -107,7 +118,20 @@ $(document).ready(function() {
       console.log('something broke');
     };
     $('#current-player').html(displayPlayerFunction(turnNumber));
+    // needs to be condensed
+    $("#player-1-current").text(player1.score);
+    $("#player-1-total").text(player1.total);
+    $("#player-2-current").text(player2.score);
+    $("#player-2-total").text(player2.total);
   });
+  // may not be necessary
+  $("#player-1-current").text(player1.score);
+  $("#player-1-total").text(player1.total);
+  $("#player-2-current").text(player2.score);
+  $("#player-2-total").text(player2.total);
+
+
+
 });
 
 
