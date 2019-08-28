58c4fe68f467f7683962e1a7f07e79b245bd734d
brendon tse and loewy malkovich <brendon@>
2019-08-28 15:49:33 -0700

    add endturn, roll on 1 and victory check methods, needs total score display

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,16 @@ var rollDice = function() {
 // PLayer tracker
 var turnNumber = 1;
 
+var displayPlayerFunction = function(turnNumber) {
+  if (turnNumber % 2 !== 0) {
+    var displayCurrentPlayer = "Player 1's";
+  } else {
+    var displayCurrentPlayer = "Player 2's";
+  };
+  return displayCurrentPlayer;
+};
+
+
 // /  Player constructor and prototypes
 function Player (playerName) {
   this.score = 0;
@@ -28,13 +38,19 @@ Player.prototype.endTurn = function() {
    this.total += this.score;
    this.score = 0;
    turnNumber ++;
-   return this.total;
 };
 
 Player.prototype.roll1 = function() {
   this.score = 0;
   turnNumber ++;
 };
+
+Player.prototype.victory = function() {
+  if ((this.total + this.score) >= 20){
+    $('.victory').show();
+  }
+};
+
 
 // ui logic
 var player1 = new Player("player1");
@@ -50,18 +66,47 @@ $(document).ready(function() {
   $("#roll-dice").click(function(event) {
     event.preventDefault();
     var newRoll = rollDice();
+
     if (turnNumber % 2 !== 0) {
-      player1.updateCurrentScore(newRoll);
-      $("#displayDice").text(newRoll);
-    } else if (turnNumber % 2 == 0){
-      player2.updateCurrentScore(newRoll);
-      $("#displayDice").text(newRoll);
+      if (newRoll !== 1) {
+        player1.updateCurrentScore(newRoll);
+        $("#display-dice").text(newRoll);
+        player1.victory();
+      } else {
+        player1.roll1();
+        $("#display-dice").text(newRoll);
+      }
+    } else if (turnNumber % 2 == 0) {
+      if (newRoll !== 1) {
+        player2.updateCurrentScore(newRoll);
+        $("#display-dice").text(newRoll);
+        player2.victory();
+
+      } else {
+        player2.roll1();
+        $("#display-dice").text(newRoll);
+      }
     } else {
       console.log('something broke');
     };
     console.log(newRoll);
     console.log(player1);
     console.log(player2);
+    $('#current-player').html(displayPlayerFunction(turnNumber));
+  });
+
+
+  $("#end-turn").click(function(event){
+    event.preventDefault();
+    $("#display-dice").text("");
+    if (turnNumber % 2 !== 0) {
+      player1.endTurn();
+    } else if (turnNumber % 2 == 0) {
+      player2.endTurn();
+    } else {
+      console.log('something broke');
+    };
+    $('#current-player').html(displayPlayerFunction(turnNumber));
   });
 });
 
